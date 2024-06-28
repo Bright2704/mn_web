3fe83813a9ee21e3a32a15ee482e17ebef4804de
vadhin11 <vadhin11@gmail.com>
2024-06-28 18:44:31 +0700

    edit announce in dashboard

---
+++ app/user/components/Annoucement.tsx
@@ -0,0 +1,20 @@
+// components/TrackingWidget.tsx
+import React from 'react';
+
+const Announcement = () => {
+    return (
+        <div className="announment">
+            <h2>
+                ประกาศ 1
+            </h2>
+            <h2>
+                ประกาศ 2
+            </h2>
+            <h2>
+                ประกาศ 3
+            </h2>
+        </div>
+    );
+};
+
+export default Announcement;
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -3,16 +3,16 @@ import { metadata } from '../app/user/metadata';
 import React from 'react';
 import Layout from '../app/user/layout';  // Update the import path as necessary
 import TrackingWidget from '../app/user/components/TrackingWidget';  // Import your widget
-
+import Announcement from '@/app/user/components/Annoucement';
 const dashboard = () => {
     return (
         <Layout>
             <div className="dashboard">
-                <h1>Dashboard</h1>
-                <div className="content">
+                <Announcement/>
+            </div>
+            <div className="content">
                     <TrackingWidget />  {/* Use your widget here */}
                     {/* Add more widgets as needed */}
-                </div>
             </div>
         </Layout>
     );
