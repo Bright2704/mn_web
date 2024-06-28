6e031c3a564a95c3f2863e44ab90941fade8e447
vadhin11 <vadhin11@gmail.com>
2024-06-29 01:15:50 +0700

    change anouncement

--- app/user/components/Annoucement.tsx
+++ app/user/components/Annoucement.tsx
@@ -4,16 +4,11 @@ import React from 'react';
 const Announcement = () => {
     return (
         <div className="announment">
-            <h2>
-                ประกาศ 1
-            </h2>
-            <h2>
-                ประกาศ 2
-            </h2>
-            <h2>
-                ประกาศ 3
-            </h2>
+            <div className='announment-sub'>
+                    ประกาศ
+            </div>
         </div>
+        
     );
 };
 
