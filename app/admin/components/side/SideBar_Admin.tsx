b431ee6349a3f9a6227c1485d0886776b4a6bd54
Nakarin Ratsuntia <patong29149@gmail.com>
2024-10-16 15:38:39 +0700

    ปรับ sidebar เพิ่ม icon

--- app/admin/components/side/SideBar_Admin.tsx
+++ app/admin/components/side/SideBar_Admin.tsx
@@ -1,74 +1,140 @@
 "use client"; // Add this line
 
 import Link from 'next/link';
+import Image from 'next/image';
+import { useState } from 'react';
+import { FormEvent } from 'react';
 
 const SideBar2 = () => {
+
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+
+    const toggleSubmenu = (e:FormEvent) => {
+        e.preventDefault();
+        setIsSubmenuOpen(!isSubmenuOpen);
+    };
+
   return (
     <div className="sidebar">
       <ul>
         <li className="menu-item">
-          <Link href="/admin/dashboard">แดชบอร์ด</Link>
+          <Link href="/admin/dashboard">
+            <Image className='h-full mr-2 hover-icon' src="/dashboard-monitor.png" alt='dashboard image' width={14} height={10}/> 
+            แดชบอร์ด
+          </Link>
         </li>
         <li className="menu-item">
-          <Link href="/admin/settings">ตั้งค่าระบบ</Link>
+          <Link href="/admin/settings">
+            <Image className='h-full mr-2 hover-icon' src="/settings.png" alt='dashboard image' width={14} height={10}/>
+            ตั้งค่าระบบ
+          </Link>
         </li>
         <li className="menu-item">
-          <Link href="/admin/announcement">ประกาศ</Link>
+          <Link href="/admin/announcement">
+            <Image className='h-full mr-2 hover-icon' src="/bell-notification-social-media.png" alt='dashboard image' width={14} height={10}/> 
+            ประกาศ
+          </Link>
         </li>
         <li className="menu-item">
-          <Link href="/admin/chat">แชท</Link>
+          <Link href="/admin/chat">
+          <Image className='h-full mr-2 hover-icon' src="/messages.png" alt='dashboard image' width={14} height={10}/> 
+          แชท
+          </Link>
         </li>
         <li className="menu-item">
-          <Link href="/admin/buylist">รายการสั่งซื้อ</Link>
+          <Link href="/admin/buylist">
+            <Image className='h-full mr-2 hover-icon' src="/shopping-cart.png" alt='dashboard image' width={14} height={10}/>
+            รายการสั่งซื้อ
+          </Link>
         </li>
         <li className="menu-item">
-          <Link href="/admin/status">เช็คสถานะสินค้า</Link>
+          <Link href="/admin/status">
+            <Image className='h-full mr-2 hover-icon' src="/file-export.png" alt='dashboard image' width={14} height={10}/>
+            เช็คสถานะสินค้า
+          </Link>
         </li>
         <li className="menu-item">
-          <Link href="/admin/export">รายการนำสินค้าออก</Link>
+          <Link href="/admin/export">
+            <Image className='h-full mr-2 hover-icon' src="/rectangle-list.png" alt='dashboard image' width={14} height={10}/>
+            รายการนำสินค้าออก
+          </Link>
         </li>
         <li className="menu-item">
-          <Link href="/admin/tracking">รหัสพัสดุ</Link>
+          <Link href="/admin/tracking">
+            <Image className='h-full mr-2 hover-icon' src="/password-alt.png" alt='dashboard image' width={14} height={10}/>
+            รหัสพัสดุ
+          </Link>
         </li>
         <li className="menu-item">
-          <Link href="/admin/lot">ล็อตสินค้า</Link>
+          <Link href="/admin/lot">
+            <Image className='h-full mr-2 hover-icon' src="/boxes.png" alt='dashboard image' width={14} height={10}/>
+            ล็อตสินค้า
+          </Link>
         </li>
         <li className="menu-item">
-          <Link href="/admin/transport">บริการขนส่งไปจีน</Link>
+          <Link href="/admin/transport">
+            <Image className='h-full mr-2 hover-icon' src="/truck-side.png" alt='dashboard image' width={14} height={10}/>
+            บริการขนส่งไปจีน
+          </Link>
         </li>
-        <li className="menu-item submenu-item">
-          <a href="#">
-            กระเป๋าเงิน <span style={{ float: 'right' }}></span>
+        <li className="menu-item submenu-item"> {/* submenu-item */}
+          <a href="#" onClick={toggleSubmenu} className='justify-between'>
+              <div className='flex items-center'>
+                  <Image
+                  className="h-full mr-2"
+                  src="/wallet.png"
+                  alt="dashboard image"
+                  width={14}
+                  height={10}
+                  />
+                  กระเป๋าเงิน 
+              </div>
+              
+              <span className="ml-auto transform transition-transform duration-300" style={{ transform: isSubmenuOpen ? 'rotate(180deg)' : 'rotate(0deg)' }}>▼</span>
           </a>
-          <ul className="submenu">
-            <li className="menu-item">
-              <Link href="/admin/wallet/deposit">เติมเงินเข้าระบบ</Link>
-            </li>
-            <li className="menu-item">
-              <Link href="/admin/wallet/withdraw">ถอนเงินจากระบบ</Link>
-            </li>
-            <li className="menu-item">
-              <Link href="/admin/wallet/transfer">โอนเงินไปจีน</Link>
-            </li>
-            <li className="menu-item">
-              <Link href="/admin/wallet/alipay">เติมเงินอลีเพย์</Link>
-            </li>
-            <li className="menu-item">
-              <Link href="/admin/wallet/taobao">ฝากจ่ายเถาเปา</Link>
-            </li>
-          </ul>
+          <div className="overflow-hidden transition-all duration-300 ease-in-out" style={{ maxHeight: isSubmenuOpen ? '200px' : '0' }}>
+            <ul className="submenu">
+              <li className="menu-item">
+                <Link href="/admin/wallet/deposit">• เติมเงินเข้าระบบ</Link>
+              </li>
+              <li className="menu-item">
+                <Link href="/admin/wallet/withdraw">• ถอนเงินจากระบบ</Link>
+              </li>
+              <li className="menu-item">
+                <Link href="/admin/wallet/transfer">• โอนเงินไปจีน</Link>
+              </li>
+              <li className="menu-item">
+                <Link href="/admin/wallet/alipay">• เติมเงินอลีเพย์</Link>
+              </li>
+              <li className="menu-item">
+                <Link href="/admin/wallet/taobao">• ฝากจ่ายเถาเปา</Link>
+              </li>
+            </ul>
+          </div>
         </li>
         <li className="menu-item">
-          <Link href="/admin/points">แต้ม</Link>
+          <Link href="/admin/points">
+            <Image className='h-full mr-2 hover-icon' src="/token.png" alt='dashboard image' width={14} height={10}/>
+            แต้ม
+          </Link>
         </li>
         <li className="menu-item">
-          <Link href="/admin/points_redeem">แลกแต้ม</Link>
+          <Link href="/admin/points_redeem">
+            <Image className='h-full mr-2 hover-icon' src="/token.png" alt='dashboard image' width={14} height={10}/>
+            แลกแต้ม
+          </Link>
         </li>
         <li className="menu-item">
-          <Link href="/admin/unowned-items">รายการสินค้าไม่มีเจ้าของ</Link>
+          <Link href="/admin/unowned-items">
+            <Image className='h-full mr-2 hover-icon' src="/person-circle-xmark.png" alt='dashboard image' width={14} height={10}/>
+            รายการสินค้าไม่มีเจ้าของ
+          </Link>
         </li>
         <li className="menu-item">
-          <Link href="/admin/settings_account">ตั้งค่าบัญชีผู้ใช้งาน</Link>
+          <Link href="/admin/settings_account">
+            <Image className='h-full mr-2 hover-icon' src="/admin-alt.png" alt='dashboard image' width={14} height={10}/>
+            ตั้งค่าบัญชีผู้ใช้งาน
+          </Link>
         </li>
       </ul>
     </div>
--- app/user/components/side/SideBar2.tsx
+++ app/user/components/side/SideBar2.tsx
@@ -1,105 +1,152 @@
 // app/components/SideBar2.tsx
 import Link from 'next/link';
+import Image from 'next/image';
+import { useState } from 'react';
+import { FormEvent } from 'react';
 // import Styles from '../../globals.css';
 // import styles from './SideBar.module.css'; // Ensure this path is correct based on your CSS file location
 
 const SideBar2 = () => {
+
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+    const [isSubmenuOpen2, setIsSubmenuOpen2] = useState(false);
+
+    const toggleSubmenu = (e:FormEvent) => {
+        e.preventDefault();
+        setIsSubmenuOpen(!isSubmenuOpen);
+    };
+
+    const toggleSubmenu2 = (e:FormEvent) => {
+        e.preventDefault();
+        setIsSubmenuOpen2(!isSubmenuOpen2);
+    };
+
     return (
         <div className="sidebar">
             {/* <h1 className="text-2xl font-bold">Menu</h1> */}
             <ul>
                 <li className="menu-item">
                     <Link href="/user/dashboard">
+                        <Image className='h-full mr-2 hover-icon' src="/dashboard-monitor.png" alt='dashboard image' width={14} height={10}/> 
                         แดชบอร์ด
                     </Link>
                 </li>
                 <li className="menu-item">
                     <Link href="/user/announcement">
+                        <Image className='h-full mr-2 hover-icon' src="/bell-notification-social-media.png" alt='dashboard image' width={14} height={10}/>
                         ประกาศ
                     </Link>
                 </li>
                 <li className="menu-item">
                     <Link href="/user/buylist">
+                        <Image className='h-full mr-2 hover-icon' src="/shopping-cart.png" alt='dashboard image' width={14} height={10}/>
                         รายการสั่งซื้อ
                     </Link>
                 </li>
                 <li className="menu-item">
                     <Link href="/user/status">
+                        <Image className='h-full mr-2 hover-icon' src="/file-export.png" alt='dashboard image' width={14} height={10}/>
                         เช็คสถานะสินค้าและแจ้งนำออก
                     </Link>
                 </li>
                 <li className="menu-item">
                     <Link href="/user/export">
+                        <Image className='h-full mr-2 hover-icon' src="/rectangle-list.png" alt='dashboard image' width={14} height={10}/>
                         รายการนำสินค้าออก
                     </Link>
                 </li>
                 <li className="menu-item">
                     <Link href="/user/tracking">
+                        <Image className='h-full mr-2 hover-icon' src="/password-alt.png" alt='dashboard image' width={14} height={10}/>
                         รหัสพัสดุ
                     </Link>
                 </li>
                 <li className="menu-item">
                     <Link href="/user/service">
+                        <Image className='h-full mr-2 hover-icon' src="/truck-side.png" alt='dashboard image' width={14} height={10}/>
                         บริการขนส่งไปจีน
                     </Link>
                 </li>
-                <li className="menu-item submenu-item">
-                    <a href="#">
-                        กระเป๋าเงิน <span style={{ float: 'right' }}></span>
+                <li className="menu-item submenu-item"> {/* submenu-item */}
+                    <a href="#" onClick={toggleSubmenu} className='justify-between'>
+                        <div className='flex items-center'>
+                            <Image
+                            className="h-full mr-2"
+                            src="/wallet.png"
+                            alt="dashboard image"
+                            width={14}
+                            height={10}
+                            />
+                            กระเป๋าเงิน 
+                        </div>
+                        
+                        <span className="ml-auto transform transition-transform duration-300" style={{ transform: isSubmenuOpen ? 'rotate(180deg)' : 'rotate(0deg)' }}>▼</span>
                     </a>
-                    <ul className="submenu">
+                    <div className="overflow-hidden transition-all duration-300 ease-in-out" style={{ maxHeight: isSubmenuOpen ? '200px' : '0' }}>
+                        <ul className="submenu">
                         <li className="menu-item">
-                        <Link href="/user/wallet/deposit">เติมเงินเข้าระบบ</Link>
+                            <Link href="/user/wallet/deposit">• เติมเงินเข้าระบบ</Link>
                         </li>
                         <li className="menu-item">
-                        <Link href="/user/wallet/balance">สมุดบัญชี</Link>
+                            <Link href="/user/wallet/balance">• สมุดบัญชี</Link>
                         </li>
-                    </ul>
+                        </ul>
+                    </div>
                 </li>
                 <li className="menu-item">
                     <Link href="/user/points">
+                        <Image className='h-full mr-2 hover-icon' src="/token.png" alt='dashboard image' width={14} height={10}/>
                         สะสมแต้ม
                     </Link>
                 </li>
                 <li className="menu-item">
                     <Link href="listnoowner">
+                    <Image className='h-full mr-2 hover-icon' src="/person-circle-xmark.png" alt='dashboard image' width={14} height={10}/>
                         รายการสินค้าไม่มีเจ้าของ
                     </Link>
                 </li>
                 <li className="menu-item">
                     <Link href="/user/profile">
+                    <Image className='h-full mr-2 hover-icon' src="/admin-alt.png" alt='dashboard image' width={14} height={10}/>
                         ตั้งค่าบัญชีผู้ใช้งาน
                     </Link>
                 </li>
-                <li className="menu-item submenu-item">
-                    <a href="#">
-                    คู่มือการใช้งาน
+                <li className="menu-item submenu-item"> {/* submenu-item */}
+                    <a href="#" onClick={toggleSubmenu2} className='justify-between'>
+                        <div className='flex items-center'>  
+                            <Image className='h-full mr-2' src="/guide-alt.png" alt='dashboard image' width={14} height={10}/>
+                            คู่มือการใช้งาน 
+                        </div>
+                        <span className="ml-auto transform transition-transform duration-300" style={{ transform: isSubmenuOpen2 ? 'rotate(180deg)' : 'rotate(0deg)' }}>▼</span>
                     </a>
+                    <div className="overflow-hidden transition-all duration-300 ease-in-out" style={{ maxHeight: isSubmenuOpen2 ? '200px' : '0' }}>
                     <ul className="submenu">
                         <li className="menu-item">
                             <Link href="/user/private">
-                                วิธีแก้ไขข้อมูลส่วนตัว
+                            • วิธีแก้ไขข้อมูลส่วนตัว
                             </Link>
                         </li>
                         <li className="menu-item">
                             <Link href="/user/private">
-                                วิธีการสั่งซื้อและชำระเงิน
+                            • วิธีการสั่งซื้อและชำระเงิน
                             </Link>
                         </li>
                         <li className="menu-item">
                             <Link href="/user/private">
-                                วิธีเติมเงิน/ถอนเงินในระบบ
+                            • วิธีเติมเงิน/ถอนเงินในระบบ
                             </Link>
                         </li>
                         <li className="menu-item">
                             <Link href="/user/private">
-                                วิธีแสดงความเป็นเจ้าของฯ
+                            • วิธีแสดงความเป็นเจ้าของฯ
                             </Link>
                         </li>
                     </ul>
+                    </div>
                 </li>
                 <li className="menu-item">
                     <Link href="/user/LineNotification">
+                    <Image className='h-full mr-2 hover-icon' src="/line.png" alt='dashboard image' width={14} height={10}/>
                         เชื่อมต่อ LINE NOTIFY
                     </Link>
                 </li>
